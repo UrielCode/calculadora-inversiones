251142c12a00ace49b051bb4972ed4a17aefcb70
Uriel Rdguez <uriel.rdguez@gmail.com>
2023-06-30 14:54:43 -0600

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,7 @@ function App() {
 
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
+      
       {!userInput && <p className="textDefault">Aún no se ha calculado ninguna inversión.</p> }
       {userInput &&  <Table data={yearlyData} initialInvestment = {userInput['current-savings']} />}
 
